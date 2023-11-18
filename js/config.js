987b4856b5071e79110dfc0eef266b6dde45cf29
Katriel-Jewel <143442869+Katriel-Jewel@users.noreply.github.com>
2023-11-18 12:22:34 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId:         'us-east-2_CaVerKJ9q',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '7pgeltqhlc15qde4er2ojmu2k0 ',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId:   '7pgeltqhlc15qde4er2ojmu2k0',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
